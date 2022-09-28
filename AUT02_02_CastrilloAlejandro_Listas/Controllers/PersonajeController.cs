ebc15216988007ab4f9158374015ffa41c2462e5
2dawa <2dawa@PC23305>
2022-09-28 09:12:04 +0100

    Agregar archivos de proyecto.

---
+++ AUT02_02_CastrilloAlejandro_Listas/Controllers/PersonajeController.cs
@@ -0,0 +1,46 @@
+﻿using AUT02_02_CastrilloAlejandro_Listas.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+namespace AUT02_02_CastrilloAlejandro_Listas.Controllers
+{
+    public class PersonajeController : Controller
+    {
+        public static List<Personaje> listaperson = new List<Personaje>();
+        public IActionResult Index()
+        {
+            if (listaperson.Count == 0){ 
+                iniciarpersonajes();
+            }
+            return View(listaperson);
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Personaje perso)
+        {
+            if (ModelState.IsValid)
+            {
+                agregarpersonaje(perso.Id, perso.Name, perso.Family, perso.NChildren);
+                RedirectToAction("index");
+            }
+            return View(perso);
+        }
+
+        public void iniciarpersonajes()
+        {
+
+            agregarpersonaje(1, "primero", "no se", 0);
+            agregarpersonaje(2, "segundo", "alguien", 0);
+        }
+        public void agregarpersonaje(int Id,string Nombre,string Familia,int NChildren)
+        {
+            listaperson.Add(new Personaje(Id, Nombre, Familia, NChildren));
+        }
+    }
+}
---
+++ AUT02_02_CastrilloAlejandro_Listas/Models/Personaje.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AUT02_02_CastrilloAlejandro_Listas.Models
+{
+    public class Personaje
+    {
+        
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public int NChildren { get; set; }
+
+        public Personaje(int value1, string value2, string value3, int value4)
+        {
+            this.Id = value1;
+            this.Name = value2;
+            this.Family = value3;
+            this.NChildren = value4;
+        }
+
+    }
+
+}
